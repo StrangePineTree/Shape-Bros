776956f4ef6ec0a415bb61b6846338fb25a6f718
StrangePineTree <97989279+StrangePineTree@users.noreply.github.com>
2022-09-12 14:46:15 -0600

    Add files via upload

---
+++ super mm bros.py
@@ -0,0 +1,236 @@
+TREW = True
+Fakse = False
+import pygame
+pygame.init()
+screen = pygame.display.set_mode((1900, 900))
+pygame.display.set_caption("Super MM Bros 2")
+clock = pygame.time.Clock()
+backrooms = pygame.image.load("backrooms.png")
+
+
+#todo: fix xv being set to 0 when p2 and p1 on ground
+
+class Player:
+    LEFT = False
+    RIGHT = True
+    x = 0
+    y = 0
+    vx = 0.0
+    vy = 0.0
+    direction = RIGHT
+    ground = Fakse
+    jumps = 0
+    jumpUp = Fakse
+    damage = 0.0
+    KB = 0
+    stuned = Fakse
+    attack = False
+
+    def jump(self):
+        self.jumpUp = True
+        if(self.ground == True and self.jumps == 0):
+            self.vy=-20
+            self.jumps += 1
+        if (self.ground == Fakse and self.jumps >=1 and self.jumps <= 3):
+            self.vy = -12
+            self.jumps+=1
+        if (self.ground == False and self.jumps == 0):
+            self.vy = -12
+            self.jumps += 2
+
+players = [Player(), Player()]
+
+p1 = players[0]
+p2 = players[1]
+
+#starting positions
+p1.x = 300
+p1.y = 500
+p2.x = 800
+p2.y = 500
+
+gameOn = True
+qup = True
+
+
+class attack:
+    def __init__(self, owner: Player, lifetime: float, damage: float, kbx: float, kby: float, hitbox: pygame.Rect, hasHit: bool):
+        self.owner = owner
+        self.lifetime = lifetime
+        self.hitbox = hitbox
+        self.damage = damage
+        self.kbx = kbx
+        self.kby = kby
+        self.hasHit = False
+
+    def update(self):
+        self.lifetime -= 1 / 60
+        colliding = self.hitbox.collidelistall([pygame.Rect(p1.x, p1.y, 20, 20), pygame.Rect(p2.x, p2.y, 20, 20)]) #TODO: make this code not stupid
+        for i in colliding:
+            hitplayer = players[i]
+            if hitplayer == self.owner:
+                continue
+            if self.hasHit == False:
+                hitplayer.damage += self.damage
+                hitplayer.vx = (1 if self.owner.direction == Player.RIGHT else -1) * self.kbx * (hitplayer.damage / 100) # TODO: make knockback use vectors, based on direction, and potentially randomized
+                hitplayer.vy = -self.kby * (hitplayer.damage / 100)
+                print(self.kbx * hitplayer.damage,'x')
+                print(self.kby * hitplayer.damage, 'y')
+                self.kby = 0
+                self.kbx = 0
+                self.hasHit = True
+
+                
+class lightAttack(attack):
+    def __init__(self, owner: Player, x, y):
+        super().__init__(owner, 0.25, 5, 30, 50, pygame.Rect(x + (10 if owner.direction == Player.RIGHT else -30), y + 7, 40, 10), False)
+
+    def update(self):
+        super().update()
+
+
+
+attacks: list[lightAttack] = []
+
+#main game loop: - - - - - - - - - - - - - -
+while (gameOn == True):
+    clock.tick(60)
+    for event in pygame.event.get(): #quit game if x is pressed in top corner
+        if event.type == pygame.QUIT:
+            gameOn = Fakse
+
+
+#both player input - - - - - - - - - - - - - - - - -
+    if event.type == pygame.KEYUP:
+            #checks of keys were released
+            if event.key == pygame.K_w:
+                p1.jumpUp = Fakse
+            if event.key == pygame.K_UP:
+                p2.jumpUp = Fakse
+            if event.key == pygame.K_q:
+                qup = True
+            if event.key == pygame.K_a:
+                p1.vx = 0
+            if event.key == pygame.K_d:
+                p1.vx = 0
+            if event.key == pygame.K_LEFT:
+                p2.vx = 0
+            if event.key == pygame.K_RIGHT:
+                p2.vx = 0
+
+
+    #player 1 inputs/movment------------------------------------------------------------
+      
+      
+    keys = pygame.key.get_pressed()
+    #jump inputs
+    if keys[pygame.K_w] and p1.jumpUp == Fakse:
+        p1.jump()
+    #left right and duck inputs
+    if keys[pygame.K_s]:
+        p1.y+=5
+    if keys[pygame.K_a] and p1.vx != -7:
+        p1.vx -= (7/4)
+        p1.direction = Player.LEFT
+    if keys[pygame.K_d] and p1.vx != 7:
+        p1.vx = (7/4)
+        p1.direction = Player.RIGHT
+    if keys[pygame.K_q] and qup == TREW:
+        attacks.append(lightAttack(p1, p1.x, p1.y))
+        qup = False
+            
+    #makes you fall
+    if (p1.ground == Fakse):
+        p1.vy += 1
+    #stops you from falling
+    if(p1.ground == True):
+        vy = 0
+        
+    if (p1.stuned == Fakse):
+        p1.y += p1.vy
+        p1.x += p1.vx
+
+    if (p1.stuned == TREW and p1.ground == False):
+        p1.y = 0
+        p1.vx = 0
+
+
+    p1.ground = Fakse
+
+
+
+
+#player 2 inputs and movement - - - - - -
+    keys = pygame.key.get_pressed()
+    #jump inputs
+    if keys[pygame.K_UP] and p2.jumpUp == Fakse:
+        p2.jump()
+    #left right and duck inputs
+    if keys[pygame.K_DOWN]:
+        p2.vy+=5
+    if keys[pygame.K_LEFT] and p2.vx !=-7:
+        p2.vx -= (7/4)
+    if keys[pygame.K_RIGHT] and p2.vx !=7:
+        p2.vx += (7/4)
+
+
+    #makes you fall and move
+    if (p2.ground == Fakse):
+        p2.vy += 1
+
+    if (p2.attack == False or p2.stuned == Fakse):
+        p2.y += p2.vy
+
+    if (p2.attack == True or p2.stuned == TREW and p2.ground == False):
+        p2.y = 0
+
+    p2.x += p2.vx
+    p2.ground = Fakse
+
+
+#player 1 platforms
+    if (p1.x+5 >= 200 and p1.x-5 <= 1600 and p1.y+20 >= 690 and p1.y <710):
+        p1.ground = True
+        p1.jumps = 0
+        p1.vy = 0
+        p1.vx = 0
+        
+#player 2 platforms
+    if (p2.x+5 >= 200 and p2.x-5 <= 1600 and p2.y+20 >= 690 and p2.y <710):
+        p2.ground = True
+        p2.jumps = 0
+        p2.vy = 0
+        p2.vx = 0
+
+    for a in attacks:
+        a.update()
+        if a.lifetime <= 0:
+            attacks.remove(a)
+
+#render - - - - - - - - - - - - - - - - 
+    screen.fill((0,0,15))
+    pygame.draw.rect(screen, (100, 100, 100), (200, 690, 1400, 50))#main platform
+#attack boxes - - - - - 
+    attacksurface = pygame.Surface((screen.get_width(), screen.get_height()))
+    attacksurface.set_alpha(100)
+    for a in attacks:
+        pygame.draw.rect(screen, (200, 50, 50), a.hitbox)
+
+    screen.blit(attacksurface, (0, 0))
+#attack boxes - - - - - -
+    pygame.draw.rect(screen, (205, 50, 30), (p1.x, p1.y, 20, 20), 150)#player 1
+    pygame.draw.rect(screen, (30, 190, 50), (p2.x, p2.y, 20, 20), 150)#player 2
+    
+    font = pygame.font.Font(None, 100)
+    text = font.render(str(p1.damage), True, (150, 255, 255))
+    screen.blit(text, (1300,750))
+    text = font.render(str(p2.damage), True, (150,255,255))
+    screen.blit(text, (420,750))
+
+    #backrooms
+    if (p1.y > 2000 and p2.y >2000):
+        screen.blit(backrooms,(0,0))
+
+
+    pygame.display.flip()
+    
